333a4bf912ffde6819c9d285b6ae7acccb730a0f
slhale <sarahlauhale@gmail.com>
2015-11-10 19:45:09 -0800

    Progress on the GUI
    Started adding the layout for the input fields for
    the constraints to be applied to the WDS table. Have
    not implemented applying the user input to the WDS
    table constrain function or displaying the table.

--- WDS_GUI.py
+++ WDS_GUI.py
@@ -47,70 +47,147 @@ class HelloWorld:
     
         # Sets the border width of the window.
         self.window.set_border_width(10)
-    
         
-        ########### VBOX
-        # Create a new hbox with the appropriate homogeneous
-	    # and spacing settings
-        self.box = gtk.VBox(False, True)#(homogeneous, spacing)
+        # Make the window visible
+        self.window.show()
+        
+        
+        ########### HBOX
+        ##### Highest level box
+        ##### Left = 'old' WDS extraction tool; Right = Graphing 
+        
+        # TODO figure out what the best settings for homogenous and spacing are
+        # Create an hbox
+        self.mainHBox = gtk.HBox(False, True)#(homogeneous, spacing)
+        # Add it to the window itself
+        self.window.add(self.mainHBox)
+        self.mainHBox.show()
+        
 	    
-	    # Create a series of buttons with the appropriate settings
-	    #button = gtk.Button("box.pack")
-	    #box.pack_start(button, expand, fill, padding)
-	    #button.show()
+        ########### VBOX
+        ##### Box containing the 'old' WDS extraction tool
+        ##### Top = user inputs for constraints; Bottom = resulting WDS table
         
+        # Create a vbox
+        self.wdsVBox = gtk.VBox(False, True)#(homogeneous, spacing)
+	    # Add this vbox to the highest level hbox
+        self.mainHBox.pack_start(self.wdsVBox, True, True, False)#, expand, fill, padding)
+        self.wdsVBox.show()
         
-        ####### BUTTON 
+        ########### BUTTON 
         
         # Creates a new button with the label "Hello World".
-        self.button = gtk.Button("Hello World")
+#        self.button = gtk.Button("Hello World")
     
         # When the button receives the "clicked" signal, it will call the
         # function hello() passing it None as its argument.  The hello()
         # function is defined above.
-        self.button.connect("clicked", self.hello, None)
+#        self.button.connect("clicked", self.hello, None)
     
         # This will cause the window to be destroyed by calling
         # gtk_widget_destroy(window) when "clicked".  Again, the destroy
         # signal could come from here, or the window manager.
-        self.button.connect_object("clicked", gtk.Widget.destroy, self.window)
+#        self.button.connect_object("clicked", gtk.Widget.destroy, self.window)
     
         # This packs the button into the window (a GTK container).
         #self.window.add(self.button)
-        self.box.pack_start(self.button, False, False, False)#, expand, fill, padding)
+#        self.vbox.pack_start(self.button, False, False, False)#, expand, fill, padding)
 	    
 	    
         # The final step is to display this newly created widget.
-        self.button.show()
+#        self.button.show()
+        
+        
+        ############# OPTIONS
+        
+        # Make a table to contain all the input fields 
+        self.inputsTable = gtk.Table(rows=7, columns=6, homogeneous=False)
+        # Add the inputs table to the wds vbox container 
+        self.wdsVBox.pack_start(self.inputsTable, False, True, False)#, expand, fill, padding)
+        self.inputsTable.show()
+        
+        ### Now let's put things into the table
+        
+        ######## TIME CONSTRAINTS
+        
+        # Make title for time constraints section 
+        self.timeConstrainLabel = gtk.Label("Time constraints:")
+        
+        # Attach it to the top row of the table
+        self.inputsTable.attach(self.timeConstrainLabel, 0, 1, 0, 1)#left_attach, right_attach, top_attach, bottom_attach)
+        self.timeConstrainLabel.show()
+        
+        
+        # Make subtitles for the start and stop hour angle fields
+        self.startHALabel = gtk.Label("Start HA")
+        self.stopHALabel = gtk.Label("Stop HA")
+        
+        # Attach them to the 2nd row of the table
+        self.inputsTable.attach(self.startHALabel, 0, 1, 1, 2)#left_attach, right_attach, top_attach, bottom_attach)
+        self.startHALabel.show()
+        self.inputsTable.attach(self.stopHALabel, 1, 2, 1, 2)#left_attach, right_attach, top_attach, bottom_attach)
+        self.stopHALabel.show()
+        
+        
+        # Make input field for the user 
+        self.startHAInput = gtk.Entry()
+        self.stopHAInput = gtk.Entry()
+        
+        # Attach them to the 3rd row of the table
+        self.inputsTable.attach(self.startHAInput, 0, 1, 2, 3)#left_attach, right_attach, top_attach, bottom_attach)
+        self.startHAInput.show()
+        self.inputsTable.attach(self.stopHAInput, 1, 2, 2, 3)#left_attach, right_attach, top_attach, bottom_attach)
+        self.stopHAInput.show()
+        
+        
+        ######## STAR CONSTRAINTS
+        
+        # Make title for star constraints section 
+        self.starConstrainLabel = gtk.Label("Star constraints:")
+        
+        # Attach it to the 4th row of the table
+        self.inputsTable.attach(self.starConstrainLabel, 0, 1, 3, 4)#left_attach, right_attach, top_attach, bottom_attach)
+        self.starConstrainLabel.show()
+        
+        
+        # Make subtitles for the star options
+        self.separationLabel = gtk.Label("Separation")
+        self.magnitudeLabel = gtk.Label("Magnitude")
+        self.deltaMagLabel = gtk.Label("Delta Magnitude")
+        
+        # Attach them to the 5th row of the table
+        self.inputsTable.attach(self.separationLabel, 0, 1, 4, 5)#left_attach, right_attach, top_attach, bottom_attach)
+        self.separationLabel.show()
+        self.inputsTable.attach(self.magnitudeLabel, 1, 2, 4, 5)#left_attach, right_attach, top_attach, bottom_attach)
+        self.magnitudeLabel.show()
+        self.inputsTable.attach(self.deltaMagLabel, 2, 3, 4, 5)#left_attach, right_attach, top_attach, bottom_attach)
+        self.deltaMagLabel.show()
+        
         
         ############# TEXTVIEW
+        ####### Displays the produced WDS table
         
+        ######## TEXTBUFFER
+        ## Contains the actual text for the WDS table 
         
-        # Adding a milti-line text widget
-        self.textview = gtk.TextView(buffer=None)
+        # Make a text buffer, which contains the actual text to 
+        # go into the text view
+        self.text = gtk.TextBuffer()
+        # TODO set the text to be the WDS table, and make it updateable
+        self.text.set_text("text for the text view blah blah \n tadfhaisdufbausdf")
         
-        textbuffer = self.textview.get_buffer()
-        print textbuffer
+        # Make a text view, which is used to view the text buffer 
+        # containing the WDS table
+        self.textview = gtk.TextView(buffer = self.text)#None)
         
-        # Make this textview uneditable 
+        # Make text uneditable 
         self.textview.set_editable(False)
         
-        # Add the text to the display
-        #self.window.add(self.textview)
-        self.box.pack_start(self.textview, True, True, False)#, expand, fill, padding)
-        # Display the text widget
+        # Add the text to the wds vbox container 
+        self.wdsVBox.pack_start(self.textview, True, True, False)#, expand, fill, padding)
         self.textview.show()
         
         
-        ############### END
-        
-        # add the box to the window after we put stuff into it 
-        self.window.add(self.box)
-        self.box.show()
-        
-        # and the window
-        self.window.show()
-
     def main(self):
         # All PyGTK applications must have a gtk.main(). Control ends here
         # and waits for an event to occur (like a key press or mouse event).

8c4a1354117916dd8b8e0c41f0ab258dcde514cf
slhale <sarahlauhale@gmail.com>
2015-11-17 18:03:23 -0800

    Removed astropy module from repo
    Astropy is still required, just not being tracked here.

--- astropy-1.0.4/astropy/coordinates/tests/accuracy/__init__.py
+++
@@ -1,4 +0,0 @@
-from __future__ import (absolute_import, division, print_function,
-                        unicode_literals)
-
-N_ACCURACY_TESTS = 10  # the number of samples to use per accuracy test
--- astropy-1.0.4/licenses/README.rst
+++
@@ -1,5 +0,0 @@
-Licenses
-========
-
-This directory holds license and credit information for astropy, works astropy is derived from, and/or datasets.
-
